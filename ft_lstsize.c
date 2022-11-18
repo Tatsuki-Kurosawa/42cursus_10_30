00fe353aaa5596265942a272914a9ba132c35f3c
Tatsuki Kurosawa <kuro96831@gmail.com>
2022-11-18 20:00:06 +0900

    lstsizeupd

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: kurosawaitsuki <kurosawaitsuki@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 17:31:03 by kurosawaits       #+#    #+#             */
-/*   Updated: 2022/11/02 17:31:04 by kurosawaits      ###   ########.fr       */
+/*   Updated: 2022/11/18 19:56:05 by kurosawaits      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	ft_lstsize(t_list *lst)
 		count++;
 		lst = lst->next;
 	}
-	if (lst->next == NULL)
-		count++;
+	count++;
 	return (count);
 }
